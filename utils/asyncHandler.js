3b9c8fd92c9092022d0927800cdb815b5bfd964b
Vishal Kukde <vishalkukde11@gmail.com>
2026-04-14 18:42:13 +0530

    Added Cron

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -4,7 +4,5 @@ const asyncHandler = fn => (req, res, next) => {
 
 export default asyncHandler;
 
-
 // A higher-order function that catches async errors automatically and forwards them to your error middleware.
-
 // “Yes, asyncHandler is a higher-order function. It takes an async controller and returns a new function that automatically catches promise rejections and forwards them to Express error middleware.”
